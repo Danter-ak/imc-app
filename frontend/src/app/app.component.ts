754f47a64ad675d9d8d7815315d4e815992cf2a2
Danter-ak <akram.benamor94@gmail.com>
2025-05-27 18:28:16 -0400

    Remplacement du frontend par une nouvelle version fonctionnelle avec Angular

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,52 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
+  standalone: false,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'frontend';
+
+  taille: number = 0;
+  poids: number = 0;
+  imc: number | null = null;
+  categorie: string = '';
+
+  calculerIMC(): void {
+    if (this.taille <= 0 || this.poids <= 0) {
+      this.imc = null;
+      this.categorie = 'Veuillez entrer une taille et un poids valides';
+      return;
+    }
+
+    this.imc = this.poids / (this.taille * this.taille);
+
+    if (this.imc < 18.5) {
+      this.categorie = 'Insuffisance pondérale';
+    } else if (this.imc < 25) {
+      this.categorie = 'Poids normal';
+    } else if (this.imc < 30) {
+      this.categorie = 'Surpoids';
+    } else {
+      this.categorie = 'Obésité';
+    }
+  }
+  getCouleurCategorie(): string {
+    switch (this.categorie) {
+      case 'Insuffisance pondérale':
+        return 'blue';
+      case 'Poids normal':
+        return 'green';
+      case 'Surpoids':
+        return 'orange';
+      case 'Obésité':
+        return 'red';
+      case 'Veuillez entrer une taille et un poids valides':
+        return 'darkgray';
+      default:
+        return 'black';
+    }
+  }
 }
